59ec5827dc7ec9728e43ca0c9f9717bc46d93bd3
SnO2WMaN <me@sno2wman.net>
2022-06-20 10:38:45 +0900

    exampleを修正

--- example/flake.nix
+++ example/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.satyxin.url = "github:aumyf/satyxin";
+  inputs.satyxin.url = "path:../";
   inputs.uline = {
     url = "github:puripuri2100/SATySFi-uline";
     flake = false;
@@ -8,11 +8,21 @@
 
   outputs = { self, nixpkgs, flake-utils, satyxin, uline }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+      let pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          satyxin.overlay
+        ];
+      };
+      in
       rec {
         packages = flake-utils.lib.flattenTree {
-          uline = satyxin.lib.buildPackage { name = "satysfi-uline"; src = uline; path = "uline.satyh"; };
-          demo = satyxin.lib.buildDocument {
+          uline = pkgs.satyxin.buildPackage {
+            name = "satysfi-uline";
+            src = uline;
+            path = "uline.satyh";
+          };
+          demo = pkgs.satyxin.buildDocument {
             name = "satyxin-demo";
             src = ./.;
             filename = "demo.saty";
