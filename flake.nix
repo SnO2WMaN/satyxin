28b51a4956977445779fa8e0283e22f4ba834281
SnO2WMaN <me@sno2wman.net>
2022-06-20 10:40:22 +0900

    ルートのflake.nixから不要なものを削除

--- flake.nix
+++ flake.nix
@@ -1,46 +1,9 @@
 {
   description = "Build SATySFi documents using Nix";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-
-    uline = {
-      url = "github:puripuri2100/SATySFi-uline";
-      flake = false;
-    };
-  };
-
-  outputs = { self, nixpkgs, flake-utils, uline }:
+  outputs = { self, ... }:
     rec {
       overlays.default = (import ./overlay.nix);
       overlay = overlays.default;
-    } //
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (import ./overlay.nix)
-          ];
-        };
-        in
-        rec {
-          packages = {
-            uline = pkgs.satyxin.buildPackage {
-              name = "satysfi-uline";
-              src = uline;
-              path = "uline.satyh";
-            };
-            demo = pkgs.satyxin.buildDocument {
-              name = "satyxin-demo";
-              src = ./example;
-              filename = "demo.saty";
-              buildInputs = [ packages.uline ];
-            };
-          };
-
-          defaultPackage = packages.demo;
-        }
-      );
+    };
 }
