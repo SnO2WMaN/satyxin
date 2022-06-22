c35034d6cc908f3c5cb49cc8f21e1c378fa99c10
SnO2WMaN <me@sno2wman.net>
2022-06-22 21:55:10 +0900

    upd fss

--- pkgs/fss/default.nix
+++ pkgs/fss/default.nix
@@ -1,4 +1,34 @@
-(import ../../nix/create-pkg.nix) {
-  name = "fss";
-  path = "./src/fss/fss.satyh";
-}
+let
+  lock = builtins.fromJSON (builtins.readFile ../../flake.lock);
+in
+  {pkgs}:
+    pkgs.stdenv.mkDerivation {
+      name = "fss";
+      src = with lock.nodes."satysfi-fss".locked;
+        pkgs.fetchFromGitHub {
+          inherit owner repo rev;
+          sha256 = narHash;
+        };
+      sources = builtins.toJSON {
+        dirs = [
+          "./src/fss"
+        ];
+      };
+      installPhase = ''
+        mkdir -p $out/lib/satysfi/dist/packages/$name/
+
+        for i in $(seq 0 $(($(echo $sources | jq ".files | length") - 1))); do
+            path=`echo $sources | jq -r ".files[$i]"`
+            cp $src/$path $out/lib/satysfi/dist/packages/$name/$path
+        done
+
+        for i in $(seq 0 $(($(echo $sources | jq ".dirs | length") - 1))); do
+            path=`echo $sources | jq -r ".dirs[$i]"`
+            cp -r $src/$path/* $out/lib/satysfi/dist/packages/$name
+        done
+      '';
+      setupHook = pkgs.writeText "setuphook-satysfi-package" ''
+        SATYSFI_LIBPATH+=:$1/lib/satysfi
+      '';
+      buildInputs = with pkgs; [jq];
+    }
