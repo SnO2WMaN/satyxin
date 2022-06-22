7e4eb26b6da86d675285230b55101ac7a2028f23
SnO2WMaN <me@sno2wman.net>
2022-06-22 22:25:52 +0900

    create-pkgをsatyxinに移管

--- nix/create-pkg.nix
+++ nix/create-pkg.nix
@@ -5,29 +5,11 @@ in
     name,
     sources ? {},
   }: {pkgs}:
-    pkgs.stdenv.mkDerivation {
-      name = name;
+    pkgs.satyxin.buildPackage {
+      inherit name sources;
       src = with lock.nodes."satysfi-${name}".locked;
         pkgs.fetchFromGitHub {
           inherit owner repo rev;
           sha256 = narHash;
         };
-      sources = builtins.toJSON sources;
-      installPhase = ''
-        mkdir -p $out/lib/satysfi/dist/packages/$name/
-
-        for i in $(seq 0 $(($(echo $sources | jq ".files | length") - 1))); do
-            path=`echo $sources | jq -r ".files[$i]"`
-            cp $src/$path $out/lib/satysfi/dist/packages/$name/$path
-        done
-
-        for i in $(seq 0 $(($(echo $sources | jq ".dirs | length") - 1))); do
-            path=`echo $sources | jq -r ".dirs[$i]"`
-            cp -r $src/$path/* $out/lib/satysfi/dist/packages/$name
-        done
-      '';
-      setupHook = pkgs.writeText "setuphook-satysfi-package" ''
-        SATYSFI_LIBPATH+=:$1/lib/satysfi
-      '';
-      buildInputs = with pkgs; [jq];
     }
