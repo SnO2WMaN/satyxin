1913ca8d8db59f6f645015c3fbbf219d7f074237
SnO2WMaN <me@sno2wman.net>
2022-06-22 22:26:09 +0900

    update buildPackage

--- nix/build-package.nix
+++ nix/build-package.nix
@@ -1,12 +1,23 @@
 { pkgs }:
-{ name, src, path }:
+{ name, src, sources ? { }, }:
 pkgs.stdenv.mkDerivation {
-  inherit src name;
+  inherit name src;
+  sources = builtins.toJSON sources;
   installPhase = ''
     mkdir -p $out/lib/satysfi/dist/packages/$name/
-    cp $src/${path} $out/lib/satysfi/dist/packages/$name/
+
+    for i in $(seq 0 $(($(echo $sources | jq ".files | length") - 1))); do
+        path=`echo $sources | jq -r ".files[$i]"`
+        cp $src/$path $out/lib/satysfi/dist/packages/$name/$path
+    done
+
+    for i in $(seq 0 $(($(echo $sources | jq ".dirs | length") - 1))); do
+        path=`echo $sources | jq -r ".dirs[$i]"`
+        cp -r $src/$path/* $out/lib/satysfi/dist/packages/$name
+    done
   '';
   setupHook = pkgs.writeText "setuphook-satysfi-package" ''
     SATYSFI_LIBPATH+=:$1/lib/satysfi
   '';
+  buildInputs = with pkgs; [ jq ];
 }
