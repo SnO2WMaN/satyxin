418bb2f72b54f28e198d7e0f83ebee156e96656c
SnO2WMaN <me@sno2wman.net>
2022-06-24 01:18:30 +0900

    fss wip
    残すはfonts.satysfi-hashのマージを

--- overlay.nix
+++ overlay.nix
@@ -1,16 +1,14 @@
 final: prev: {
-  satyxinPackages = builtins.listToAttrs (
-    map (
-      name: {
-        name = name;
-        value = (import ./pkgs/${name}) {pkgs = prev;};
-      }
-    ) [
-      "base"
-      "bibyfi"
-      "fonts-junicode"
-      "fss"
-      "uline"
-    ]
-  );
+  satyxinPackages = rec {
+    base = (import ./pkgs/base) {pkgs = prev;};
+    bibyfi = (import ./pkgs/bibyfi) {pkgs = prev;};
+    uline = (import ./pkgs/uline) {pkgs = prev;};
+    fonts-junicode = (import ./pkgs/fonts-junicode) {pkgs = prev;};
+    fss = (import ./pkgs/fss) {
+      pkgs = prev;
+      deps = [
+        fonts-junicode
+      ];
+    };
+  };
 }
