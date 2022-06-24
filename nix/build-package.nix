ed3056a11ffd86eb5ec4f9fa6dc23b86fe303e27
SnO2WMaN <me@sno2wman.net>
2022-06-24 10:07:27 +0900

    update build package for dependencies

--- nix/build-package.nix
+++ nix/build-package.nix
@@ -1,10 +1,18 @@
 { pkgs }:
-{ name, src, sources ? { }, }:
+{ name, src, sources ? { }, deps ? [ ] }:
 pkgs.stdenv.mkDerivation {
-  inherit name src;
+  inherit name src deps;
   sources = builtins.toJSON sources;
   installPhase = ''
+    mkdir -p $out/lib/satysfi/dist/{fonts,hash,packages}
     mkdir -p $out/lib/satysfi/dist/packages/$name/
+
+    for dep in $(echo $deps | tr ' ' '\n'); do
+      if [ -d $dep/lib/satysfi/dist/packages ]; then
+        mkdir -p $out/lib/satysfi/dist/packages
+        cp -r $dep/lib/satysfi/dist/packages/* $out/lib/satysfi/dist/packages
+      fi
+    done
 
     for i in $(seq 0 $(($(echo $sources | jq ".files | length") - 1))); do
         path=`echo $sources | jq -r ".files[$i]"`
