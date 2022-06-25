ba22d7181ac12c3c383a599b0ed354d846fe2187
SnO2WMaN <me@sno2wman.net>
2022-06-26 00:59:28 +0900

    fonts-dejavuの諸問題修正

--- pkgs/fonts-dejavu/default.nix
+++ pkgs/fonts-dejavu/default.nix
@@ -5,7 +5,7 @@ pkgs.stdenv.mkDerivation {
     (pkgs.fetchzip {
       name = "dejavu";
       url = http://sourceforge.net/projects/dejavu/files/dejavu/2.37/dejavu-fonts-ttf-2.37.zip;
-      sha256 = "sha256-JM75TClUBnQ7qKMHXbOD96ZoTxOhBFnsN5xH0QSafjc=";
+      sha256 = "sha256-eUV9CThmZLXMosUjvwvMwnWtZpgwFGZZws848kcOgE4=";
     })
     ./fonts.satysfi-hash
     ./mathfonts.satysfi-hash
@@ -16,35 +16,13 @@ pkgs.stdenv.mkDerivation {
     done
   '';
   installPhase = ''
-    fontsrc=$(echo $src | tr ' ' '\n' | grep dejavu)/dejavu-fonts-ttf-2.37/ttf
-    fontdir=$out/lib/satysfi/dist/fonts/dejavu
+    mkdir -p $out/lib/satysfi/dist/fonts/fonts-dejavu
 
-    mkdir -p $fontdir
+    for file in $(find $(echo $src | tr ' ' '\n' | grep -E ".*-dejavu")/ttf -name "*.ttf"); do
+        cp $file $out/lib/satysfi/dist/fonts/fonts-dejavu
+    done
+
     mkdir -p $out/lib/satysfi/dist/hash
-
-    cp $fontsrc/DejaVuMathTeXGyre.ttf $fontdir
-    cp $fontsrc/DejaVuSans-Bold.ttf $fontdir
-    cp $fontsrc/DejaVuSans-BoldOblique.ttf $fontdir
-    cp $fontsrc/DejaVuSans-ExtraLight.ttf $fontdir
-    cp $fontsrc/DejaVuSans-Oblique.ttf $fontdir
-    cp $fontsrc/DejaVuSans.ttf $fontdir
-    cp $fontsrc/DejaVuSansCondensed-Bold.ttf $fontdir
-    cp $fontsrc/DejaVuSansCondensed-BoldOblique.ttf $fontdir
-    cp $fontsrc/DejaVuSansCondensed-Oblique.ttf $fontdir
-    cp $fontsrc/DejaVuSansCondensed.ttf $fontdir
-    cp $fontsrc/DejaVuSansMono-Bold.ttf $fontdir
-    cp $fontsrc/DejaVuSansMono-BoldOblique.ttf $fontdir
-    cp $fontsrc/DejaVuSansMono-Oblique.ttf $fontdir
-    cp $fontsrc/DejaVuSansMono.ttf $fontdir
-    cp $fontsrc/DejaVuSerif-Bold.ttf $fontdir
-    cp $fontsrc/DejaVuSerif-BoldItalic.ttf $fontdir
-    cp $fontsrc/DejaVuSerif-Italic.ttf $fontdir
-    cp $fontsrc/DejaVuSerif.ttf $fontdir
-    cp $fontsrc/DejaVuSerifCondensed-Bold.ttf $fontdir
-    cp $fontsrc/DejaVuSerifCondensed-BoldItalic.ttf $fontdir
-    cp $fontsrc/DejaVuSerifCondensed-Italic.ttf $fontdir
-    cp $fontsrc/DejaVuSerifCondensed.ttf $fontdir
-
     cp $(echo $src | tr ' ' '\n' | grep -E "*-fonts.satysfi-hash") $out/lib/satysfi/dist/hash/fonts.satysfi-hash
     cp $(echo $src | tr ' ' '\n' | grep -E "*-mathfonts.satysfi-hash") $out/lib/satysfi/dist/hash/mathfonts.satysfi-hash
   '';
