dd3c207147121d74c2ff565a1a06d1a84078bdae
SnO2WMaN <me@sno2wman.net>
2022-06-26 16:29:29 +0900

    default fonts周り修正

--- nix/build-package/default.nix
+++ nix/build-package/default.nix
@@ -10,16 +10,9 @@ pkgs.stdenv.mkDerivation {
 
   installPhase = ''
     for dep in $(echo $deps | tr ' ' '\n'); do
-      if [ -d $dep/lib/satysfi/dist/fonts ]; then
-        mkdir -p $out/lib/satysfi/dist/fonts
-        cp -r $dep/lib/satysfi/dist/fonts $out/lib/satysfi/dist
-      fi
-
-      if [ -e $dep/lib/satysfi/dist/hash/default-font.satysfi-hash ]; then
+      if [ -e $dep/lib/satysfi/dist/hash/default-font.satysfi-hash ] && [ ! -e $out/lib/satysfi/dist/hash/default-font.satysfi-hash ]; then
         mkdir -p $out/lib/satysfi/dist/hash
-        target=$out/lib/satysfi/dist/hash/default-font.satysfi-hash
-        [[ ! -e $target ]] && touch $target
-        cat <(cat $dep/lib/satysfi/dist/hash/default-font.satysfi-hash) $target | sponge $target
+        cat $dep/lib/satysfi/dist/hash/default-font.satysfi-hash > $out/lib/satysfi/dist/hash/default-font.satysfi-hash
       fi
 
       if [ -e $dep/lib/satysfi/dist/hash/fonts.satysfi-hash ]; then
@@ -36,9 +29,26 @@ pkgs.stdenv.mkDerivation {
         merge-hash $dep/lib/satysfi/dist/hash/mathfonts.satysfi-hash $target | sponge $target
       fi
 
+      if [ -d $dep/lib/satysfi/dist/fonts ]; then
+        mkdir -p $out/lib/satysfi/dist/fonts
+        for file in $(find $dep/lib/satysfi/dist/fonts -type f); do
+          target=$out/$(realpath --relative-to=$dep $file)
+          if [ ! -e $target ]; then
+            mkdir -p $out/$(dirname $(realpath --relative-to=$dep $file))
+            cp $file $target
+          fi
+        done
+      fi
+
       if [ -d $dep/lib/satysfi/dist/unidata ]; then
         mkdir -p $out/lib/satysfi/dist/unidata
-        cp -r $dep/lib/satysfi/dist/unidata $out/lib/satysfi/dist
+        for file in $(find $dep/lib/satysfi/dist/fonts -type f); do
+          target=$out/$(realpath --relative-to=$dep $file)
+          if [ ! -e $target ]; then
+            mkdir -p $out/$(dirname $(realpath --relative-to=$dep $file))
+            cp $file $target
+          fi
+        done
       fi
 
       if [ -d $dep/lib/satysfi/dist/packages ]; then
