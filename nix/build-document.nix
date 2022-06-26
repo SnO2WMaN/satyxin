bc84e2ba84c16c15c0b8c8fe6caff8f9ebe763ae
SnO2WMaN <me@sno2wman.net>
2022-06-26 15:27:53 +0900

    buildDocumentの出力ファイル名を指定できるように

--- nix/build-document.nix
+++ nix/build-document.nix
@@ -1,17 +1,25 @@
-{pkgs}: {
-  name,
-  src,
-  filename,
-  buildInputs ? [],
-}:
-pkgs.stdenv.mkDerivation {
-  inherit src name;
-  buildPhase = ''
-    satysfi ${filename} --output document.pdf --config $SATYSFI_LIBPATH
-  '';
-  installPhase = ''
-    mkdir -p $out/docs
-    cp document.pdf $out/docs/
-  '';
-  buildInputs = [pkgs.satysfi] ++ buildInputs;
-}
+{pkgs}: let
+  inherit (pkgs.lib) removeSuffix hasSuffix;
+in
+  {
+    name,
+    src,
+    filename,
+    output ? "${removeSuffix ".saty" filename}.pdf",
+    buildInputs ? [],
+  }:
+    pkgs.stdenv.mkDerivation {
+      inherit src name;
+      buildPhase = ''
+        satysfi ${
+          if hasSuffix ".saty" filename
+          then filename
+          else builtins.abort "File name must have '.saty', but passed '${filename}'"
+        } --output ${output} --config $SATYSFI_LIBPATH
+      '';
+      installPhase = ''
+        mkdir -p $out/docs
+        cp ${output} $out/docs/
+      '';
+      buildInputs = [pkgs.satysfi] ++ buildInputs;
+    }
