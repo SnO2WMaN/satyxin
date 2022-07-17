5d5544c5f27204b9822c90a1b2a2a4f177aff9b5
SnO2WMaN <me@sno2wman.net>
2022-07-17 20:09:33 +0900

    satysfiPackageでも利用出来るように

--- overlay.nix
+++ overlay.nix
@@ -1,41 +1,46 @@
-final: prev: {
-  satysfi-formatter = final.callPackage ./tools/satysfi-formatter {};
-  satysfi-language-server = final.callPackage ./tools/satysfi-language-server {};
+final: prev: (
+  {
+    satysfi-formatter = final.callPackage ./tools/satysfi-formatter {};
+    satysfi-language-server = final.callPackage ./tools/satysfi-language-server {};
 
-  satyxin = import ./nix {pkgs = final;};
-  satyxinPackages = let
-    satyxinPkgs = builtins.listToAttrs (
-      map (name: {
-        name = name;
-        value = (import ./pkgs/${name}) {
-          pkgs = final;
-          satyxinPkgs = satyxinPkgs;
-        };
-      }) [
-        "algorithm"
-        "azmath"
-        "base"
-        "bibyfi"
-        "chemfml"
-        "class-jlreq"
-        "class-slydifi"
-        "code-printer"
-        "derive"
-        "dist"
-        "easytable"
-        "enumitem"
-        "figbox"
-        "fonts-dejavu"
-        "fonts-junicode"
-        "fss"
-        "karnaugh"
-        "pagenumber"
-        "pagestyle"
-        "railway"
-        "ruby"
-        "uline"
-      ]
-    );
-  in
-    satyxinPkgs;
-}
+    satyxin = import ./nix {pkgs = final;};
+  }
+  // rec {
+    satysfiPackages = satyxinPackages;
+    satyxinPackages = let
+      satyxinPkgs = builtins.listToAttrs (
+        map (name: {
+          name = name;
+          value = (import ./pkgs/${name}) {
+            pkgs = final;
+            satyxinPkgs = satyxinPkgs;
+          };
+        }) [
+          "algorithm"
+          "azmath"
+          "base"
+          "bibyfi"
+          "chemfml"
+          "class-jlreq"
+          "class-slydifi"
+          "code-printer"
+          "derive"
+          "dist"
+          "easytable"
+          "enumitem"
+          "figbox"
+          "fonts-dejavu"
+          "fonts-junicode"
+          "fss"
+          "karnaugh"
+          "pagenumber"
+          "pagestyle"
+          "railway"
+          "ruby"
+          "uline"
+        ]
+      );
+    in
+      satyxinPkgs;
+  }
+)
