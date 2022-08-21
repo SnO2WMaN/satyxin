eb3afcdb9faf17ccc7da2285285388e2b9c06d12
SnO2WMaN <me@sno2wman.net>
2022-08-22 07:13:47 +0900

    add zrbase to base

--- pkgs/base/default.nix
+++ pkgs/base/default.nix
@@ -1,11 +1,18 @@
 (import ../create-pkg.nix) (
-  {inputs, ...}: {
+  {
+    pkgs,
+    inputs,
+    ...
+  }: {
     name = "base";
     version = inputs.pkg-satysfi-base.rev;
     sources = let
       root = inputs.pkg-satysfi-base;
     in [
       "${root}/src"
+    ];
+    deps = with pkgs.satyxinPackages; [
+      zrbase
     ];
   }
 )
