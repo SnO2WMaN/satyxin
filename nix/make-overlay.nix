702f8e289b5e8fc8fde297efdf0311caba5ae443
SnO2WMaN <me@sno2wman.net>
2022-08-21 13:39:26 +0900

    rename `make-overlay.nix`

---
+++ nix/make-overlay.nix
@@ -0,0 +1,40 @@
+inputs: final: prev: {
+  satysfi-formatter = final.callPackage ../tools/satysfi-formatter {};
+  satysfi-formatter-each = final.callPackage ../tools/satysfi-formatter/each.nix {};
+  satysfi-language-server = final.callPackage ../tools/satysfi-language-server {};
+
+  satyxin = import ./. {pkgs = final;};
+
+  satyxinPackages = with final.lib; (
+    listToAttrs (
+      map (name: {
+        name = name;
+        value = final.callPackage (import ../pkgs/${name}) {};
+      }) [
+        "algorithm"
+        "azmath"
+        "base"
+        "bibyfi"
+        "chemfml"
+        "class-jlreq"
+        "class-slydifi"
+        "class-stjarticle"
+        "code-printer"
+        "derive"
+        "dist"
+        "easytable"
+        "enumitem"
+        "figbox"
+        "fonts-dejavu"
+        "fonts-junicode"
+        "fss"
+        "karnaugh"
+        "pagenumber"
+        "pagestyle"
+        "railway"
+        "ruby"
+        "uline"
+      ]
+    )
+  );
+}
